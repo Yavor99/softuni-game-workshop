6906c2a4785fb5b2c7c07d88f4fe57af76dfd131
Yavor99 <yavor_enchev99@abv.bg>
2023-03-04 19:05:42 +0200

    Add comment to game state

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,20 @@ import './App.css';
 function App() {
     const [games, setGames] = useState([]);
 
+    const addComment = (gameId, comment) => {
+        setGames(state => {
+            const game = state.find(x => x._id == gameId);
+
+            const comments = game.comments || [];
+            comments.push(comment)
+
+            return [
+                ...state.filter(x => x._id !== gameId),
+                {...game, comments}
+            ]
+        })
+    }
+
     useEffect(() => {
         gameService.getAll()
             .then(result => {
@@ -33,8 +47,8 @@ function App() {
                     <Route path="/login" element={<Login />}/>
                     <Route path="/register" element={<Register />}/>
                     <Route path="/create" element={<CreateGame />}/>
-                    <Route path="/catalog" element={<Catalog games={games}/>}/>
-                    <Route path="/catalog/:gameId" element={<GameDetails/>}/>
+                    <Route path="/catalog" element={<Catalog games={games} />}/>
+                    <Route path="/catalog/:gameId" element={<GameDetails games={games} />}/>
                 </Routes>                
             </main>
                                     
