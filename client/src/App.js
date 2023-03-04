eb37741ada91812904f9416facf1e88feeb5ac51
Yavor99 <yavor_enchev99@abv.bg>
2023-03-04 18:22:05 +0200

    Add default details page

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,9 @@ import { Header } from './components/header/Header';
 import { Home } from './components/home/Home';
 import { Login } from './components/login/Login';
 import { Register } from './components/register/Register';
+import { GameDetails } from './components/gameDetails/GameDetails';
 import './App.css';
+
 
 function App() {
     const [games, setGames] = useState([]);
@@ -32,6 +34,7 @@ function App() {
                     <Route path="/register" element={<Register />}/>
                     <Route path="/create" element={<CreateGame />}/>
                     <Route path="/catalog" element={<Catalog games={games}/>}/>
+                    <Route path="/catalog/:gameId" element={<GameDetails/>}/>
                 </Routes>                
             </main>
                                     
@@ -64,64 +67,7 @@ function App() {
                 </form>
             </section> */}
             {/*Details Page*/}
-            {/* <section id="game-details">
-                <h1>Game Details</h1>
-                <div className="info-section">
-                    <div className="game-header">
-                        <img className="game-img" src="images/MineCraft.png" />
-                        <h1>Bright</h1>
-                        <span className="levels">MaxLevel: 4</span>
-                        <p className="type">Action, Crime, Fantasy</p>
-                    </div>
-                    <p className="text">
-                        Set in a world where fantasy creatures live side by side with humans. A
-                        human cop is forced to work with an Orc to find a weapon everyone is
-                        prepared to kill for. Set in a world where fantasy creatures live side
-                        by side with humans. A human cop is forced to work with an Orc to find a
-                        weapon everyone is prepared to kill for.
-                    </p>
-                    
-                    <div className="details-comments">
-                        <h2>Comments:</h2>
-                        <ul>
-                            
-                            <li className="comment">
-                                <p>Content: I rate this one quite highly.</p>
-                            </li>
-                            <li className="comment">
-                                <p>Content: The best game.</p>
-                            </li>
-                        </ul>
-                        
-                        <p className="no-comment">No comments.</p>
-                    </div>
-                   
-                    <div className="buttons">
-                        <a href="#" className="button">
-                            Edit
-                        </a>
-                        <a href="#" className="button">
-                            Delete
-                        </a>
-                    </div>
-                </div>
-                
-                 <article className="create-comment">
-                    <label>Add new comment:</label>
-                    <form className="form">
-                        <textarea
-                            name="comment"
-                            placeholder="Comment......"
-                            defaultValue={""}
-                        />
-                        <input
-                            className="btn submit"
-                            type="submit"
-                            defaultValue="Add Comment"
-                        />
-                    </form>
-                </article>
-            </section> */}
+            
             {/* Catalogue */}
            
         </div>
